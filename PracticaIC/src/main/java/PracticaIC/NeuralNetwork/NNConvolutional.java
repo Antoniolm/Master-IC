80118b188cdfba00a808581925b85d2b26a46b38
Antoniolm <codiak19@gmail.com>
2017-11-26 19:26:55 +0100

    Added the first idea of convolutional NN

--- PracticaIC/src/main/java/PracticaIC/NeuralNetwork/NNConvolutional.java
+++ PracticaIC/src/main/java/PracticaIC/NeuralNetwork/NNConvolutional.java
@@ -19,23 +19,116 @@
 
 package PracticaIC.NeuralNetwork;
 
+import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.LearningRatePolicy;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class NNConvolutional extends NeuralNetwork {
 
-    public NNConvolutional(int rngSeed,int numRows, int numColumns,int outputNum){
+    public NNConvolutional(int inputNum,int outputNum){
+        input=inputNum;
+        output=outputNum;
 
+        MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder()
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .learningRate(.01)
+                .updater(Updater.NESTEROVS)
+                .list()
+                .layer(0, new ConvolutionLayer.Builder(5, 5)
+                        .nIn(1)
+                        .stride(1, 1)
+                        .nOut(100)
+                        .activation(Activation.IDENTITY)
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .layer(1, new SubsamplingLayer.Builder()
+                        .kernelSize(2,2)
+                        .stride(2,2)
+                        .build())
+                .layer(2, new ConvolutionLayer.Builder(5, 5)
+                        .stride(1, 1)
+                        .nOut(100)
+                        .activation(Activation.IDENTITY)
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .layer(3, new SubsamplingLayer.Builder()
+                        .kernelSize(2,2)
+                        .stride(2,2)
+                        .build())
+                .layer(4, new DenseLayer.Builder()
+                        .nOut(100)
+                        .activation(Activation.RELU)
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .layer(5, new OutputLayer.Builder()
+                        .nOut(outputNum)
+                        .activation(Activation.SOFTMAX)
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .setInputType(InputType.convolutionalFlat(28,28,1)) //See note below
+                .pretrain(false).backprop(true)
+                .build();
+
+                network = new MultiLayerNetwork(configuration);
+                network.init();
+
+                /*
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .learningRate(0.05)//.biasLearningRate(0.02)
+                .updater(Updater.NESTEROVS) //To configure: .updater(new Nesterovs(0.9))
+                .list()
+                .layer(0, new ConvolutionLayer.Builder(5, 5)
+                        //nIn and nOut specify depth. nIn here is the nChannels and nOut is the number of filters to be applied
+                        .nIn(1)
+                        .stride(1, 1)
+                        .nOut(20)
+                        .activation(Activation.IDENTITY)
+                        .weightInit(WeightInit.UNIFORM)
+                        .build())
+                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(2,2)
+                        .stride(2,2)
+                        .build())
+                .layer(2, new ConvolutionLayer.Builder(5, 5)
+                        //Note that nIn need not be specified in later layers
+                        .stride(1, 1)
+                        .nOut(50)
+                        .activation(Activation.IDENTITY)
+                        .weightInit(WeightInit.UNIFORM)
+                        .build())
+                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(2,2)
+                        .stride(2,2)
+                        .build())
+                .layer(4, new DenseLayer.Builder().activation(Activation.RELU)
+                        .nOut(50).build())
+                .layer(5, new OutputLayer.Builder()
+                        .nOut(outputNum)
+                        .activation(Activation.SOFTMAX)
+                        .weightInit(WeightInit.UNIFORM)
+                        .build())
+                .setInputType(InputType.convolutionalFlat(28,28,1)) //See note below
+                .backprop(true).pretrain(false).build();*/
     }
+
 }
